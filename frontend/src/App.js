6862ab369bd9e880fa6237451a44947073756964
alihassan-here <alihassanuog50@gmail.com>
2022-02-15 23:08:06 +0500

    basic route setup with react redux template

---
+++ frontend/src/App.js
@@ -0,0 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+//PAGES
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
+import Register from './pages/Register';
+//COMPONENTS
+import Header from './components/Header';
+
+function App() {
+  return (
+    <>
+      <Router>
+        <div className='container'>
+          <Header />
+          <Routes>
+            <Route path='/' element={<Dashboard />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
+  );
+}
+
+export default App;
