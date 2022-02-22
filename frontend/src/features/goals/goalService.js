0e64474e9eace4ca768620de940b5ab59987b056
alihassan-here <alihassanuog50@gmail.com>
2022-02-22 22:32:12 +0500

    App Finished

--- frontend/src/features/goals/goalService.js
+++ frontend/src/features/goals/goalService.js
@@ -1,33 +1,50 @@
-import axios from 'axios';
-const API_URL = '/api/goals/';
+import axios from 'axios'
 
-//create new goal
+const API_URL = '/api/goals/'
+
+// Create new goal
 const createGoal = async (goalData, token) => {
-    const config = {
-        headers: {
-            Authorization: `Bearer ${token}`
-        }
-    }
-    const response = await axios.post(API_URL, goalData, config);
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  }
 
-    return response.data
+  const response = await axios.post(API_URL, goalData, config)
+
+  return response.data
 }
 
-//get user goals
+// Get user goals
 const getGoals = async (token) => {
-    const config = {
-        headers: {
-            Authorization: `Bearer ${token}`
-        }
-    }
-    const response = await axios.get(API_URL, config);
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  }
 
-    return response.data
+  const response = await axios.get(API_URL, config)
+
+  return response.data
+}
+
+// Delete user goal
+const deleteGoal = async (goalId, token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  }
+
+  const response = await axios.delete(API_URL + goalId, config)
+
+  return response.data
 }
 
 const goalService = {
-    createGoal,
-    getGoals
+  createGoal,
+  getGoals,
+  deleteGoal,
 }
 
-export default goalService;
+export default goalService
